648381b6d383a2d137456166cc8f0db16808bdbd
Jacob Wadowski <jacobwadowski@gmail.com>
2022-10-10 16:43:05 -0700

    testing dbt git connection

---
+++ models/bronze_ga.sql
@@ -0,0 +1,18 @@
+with a_data as (
+  SELECT 
+    PARSE_JSON(_airbyte_data) as json_data
+  FROM miir-testing.google_analytics.cf_website_bronze_tbl
+)
+
+SELECT
+  json_data.ga_continent,
+  json_data.ga_subcontinent,
+  json_data.ga_countryIsoCode,
+  json_data.ga_region,
+  json_data.ga_regionIsoCode,
+  json_data.ga_city,
+  json_data.ga_data,
+  json_data.ga_sessions,
+  json_data.ga_transactions,
+  json_data.ga_transactionRevenue
+FROM a_data
