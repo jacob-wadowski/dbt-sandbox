1e55a60584124918613c14be66fb3cc086d59da1
jacob-wadowski <jacobwadowski@gmail.com>
2022-10-13 16:21:43 -0700

    Fixing column name in bronze_ga sql model

--- models/bronze_ga.sql
+++ models/bronze_ga.sql
@@ -11,7 +11,7 @@ SELECT
   json_data.ga_region,
   json_data.ga_regionIsoCode,
   json_data.ga_city,
-  json_data.ga_data,
+  json_data.ga_date,
   json_data.ga_sessions,
   json_data.ga_transactions,
   json_data.ga_transactionRevenue
